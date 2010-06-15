172344844aca465b9c05deb874d2b4879006ad1d
Samuele Kaplun <Samuele.Kaplun@cern.ch>
2010-06-15 17:24:54 +0200

    bibdocfile: backport extension guessing algorithm
    * Fix extension guessing algorithm by backporting latest version from
      master. Previous algorithm was guessing "foo.docx" as having extension
      "ocx". This is fixed.

--- modules/websubmit/lib/bibdocfile.py
+++ modules/websubmit/lib/bibdocfile.py
@@ -75,7 +75,7 @@ from datetime import datetime
 from mimetypes import MimeTypes
 from thread import get_ident
 
-from invenio import webinterface_handler_config as apache
+from invenio import webinterface_handler_wsgi_utils as apache
 
 ## Let's set a reasonable timeout for URL request (e.g. FFT)
 socket.setdefaulttimeout(40)
